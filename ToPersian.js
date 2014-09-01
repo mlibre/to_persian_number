5d392485756351a5db057e8969aa57aee3529534
masoud ghorbanzadeh <m.gh@linuxmail.org>
2014-09-02 02:23:22 +0430

    Update ToPersian.js

--- ToPersian.js
+++ ToPersian.js
@@ -3,7 +3,6 @@ function ToPersian(string)
     var etp = ['۰' , '۱' , '۲' , '۳' , '۴' , '۵' , '۶' , '۷' , '۸' , '۹'];
     var sr = "";
     var st = string.toString();
-    alert(st);
     for(var i = 0 ; i < st.length ; i++)
     {
         sr += etp[ parseInt(st[i]) ];
